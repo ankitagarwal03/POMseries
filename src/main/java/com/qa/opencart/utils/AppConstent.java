a8c03755f88cd9d22cd4c3a0cb401f88e88b6231
ankit <nkt.agarwal3@gmail.com>
2023-06-06 23:01:06 +0400

    added allure

--- src/main/java/com/qa/opencart/utils/AppConstent.java
+++ src/main/java/com/qa/opencart/utils/AppConstent.java
@@ -4,7 +4,7 @@ public class AppConstent {
 
     public static final String LOGIN_PAGE_TITLE = "Account Login";
 
-    public static final String FORGOTTON_PASSWORD_TEXT = "Forgotten Password";
+    public static final  String FORGOTTON_PASSWORD_TEXT = "Forgotten Password";
 
     public static final String MY_ACCOUNT_PAGE_TITLE = "My Account";
     public static final String LOGOUT_TEXT = "Logout";
